62935c5adfcbe7b60e4abad408ee9201ee2472f4
Jaisen Mathai <jaisen@jmathai.com>
2011-11-30 12:21:10 -0800

    Adding get started page.

--- src/templates/get-started.php
+++ src/templates/get-started.php
@@ -1,1 +1,40 @@
-get started
+<h1>2 easy ways to get started</h1>
+
+<div class="get-started-block">
+  <h2>Option 1: sign up for an account</h2>
+
+  <div class="span8">
+    Signing up for an account is the easiest way to start using OpenPhoto. 
+    There's not much you need to do other than click the giant red button below.
+  </div>
+
+  <form method="get" action="http://openphoto.me">
+    <button class="btn large danger create-account" type="submit">
+      <div class="primary">Create your account now!</div>
+      <div class="secondary">It's free and easy</div>
+    </button>
+  </form>
+</div>
+
+<div class="get-started-block">
+  <h2>Option 2: install the software yourself</h2>
+
+  <div class="span8">
+    If you're a nerd who has your own server or hosting account then we've got you hooked up.
+    We've got deailed guides for he following systems and a fancy one line installer.
+    <p>
+      <ul>
+        <li><a href="/documentation/guide/InstallationUbuntuApache">Ubuntu and Apache</a></li>
+        <li><a href="/documentation/guide/InstallationOSXMacports">OSX and Macports</a></li>
+        <li><a href="/documentation/guide/InstallationSUSE">SUSE and Apache</a></li>
+        <li><a href="/documentation/guide/InstallationUsingOpenPhotoEC2-AMI">AWS EC2 using an AMI</a></li>
+      </ul>
+    </p>
+  </div>
+  <pre><code># Installing OpenPhoto on Ubuntu and Apache
+# Run this from the command line as root.
+# As always, view any script before running it ;).
+
+curl https://raw.github.com/openphoto/frontend/master/documentation/guides/InstallationUbuntuApache.sh | /bin/bash</code></pre>
+
+</div>
