53e396d3b5ef4f1995dfc558b2a7837873ec1563
Jaisen Mathai <jaisen@jmathai.com>
2012-09-11 02:14:33 -0700

    Fixing Github cron script by using v3 API. Closes #22

--- src/scripts/github.php
+++ src/scripts/github.php
@@ -5,20 +5,22 @@
   chdir(dirname(__FILE__));
   date_default_timezone_set('America/Los_Angeles');
 
-  $gUrl = 'http://github.com/api/v2/json';
-  $repoUrl = "{$gUrl}/repos/show/photo/frontend";
-  $commitsUrl = "{$gUrl}/commits/list/photo/frontend/development";
+  $gUrl = 'https://api.github.com';
+  $repoUrl = "{$gUrl}/repos/photo/frontend";
   $ch = curl_init($repoUrl);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
   $repoJson = curl_exec($ch);
   curl_close($ch);
+
+  $commitsUrl = "{$repoUrl}/commits";
   $ch = curl_init($commitsUrl);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
   $commitsJson = curl_exec($ch);
+
   $repo = json_decode($repoJson, 1);
-  $repo = $repo['repository'];
   $commits = json_decode($commitsJson, 1);
-  if(empty($repo) || empty($commits['commits']))
+
+  if(empty($repo) || empty($commits))
     die();
 
   $out = array();
@@ -28,22 +30,22 @@
     'forks' => $repo['forks']
   );
   $out['commits'] = array();
-  foreach($commits['commits'] as $cnt => $commit)
+  foreach($commits as $cnt => $commit)
   {
     if($cnt > 4)
       break;
 
-    $time = strtotime($commit['committed_date']);
+    $time = strtotime($commit['commit']['author']['date']);
     if($time < strtotime('Today'))
       $timefmt = date('m/d/y', $time);
     else
       $timefmt = date('g:ia', $time);
 
     $out['commits'][] = array(
-      'message' => htmlspecialchars($commit['message']),
-      'messagefmt' => htmlspecialchars(substr($commit['message'], 0, 15)).'...',
+      'message' => htmlspecialchars($commit['commit']['message']),
+      'messagefmt' => htmlspecialchars(substr($commit['commit']['message'], 0, 15)).'...',
       'name' => $commit['author']['login'],
-      'email' => $commit['author']['email'],
+      'email' => $commit['commit']['author']['email'],
       'time' => $time,
       'timefmt' => $timefmt,
       'url' => $commit['url']
